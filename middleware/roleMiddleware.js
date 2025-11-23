da9a1f14ec7583741a8608f15cedbf40e58d3003
unknown <117914221+RwtAditya@users.noreply.github.com>
2025-11-23 19:21:07 +0530

    Added middleware for access to routes based on role

---
+++ middleware/roleMiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+exports.requireRole = (allowedRoles) => {
+    return (req,res, next) => {
+        const role = req.user.role;
+        if(role === "admin") {
+            return next();
+        }
+        if(!allowedRoles.includes(role)) {
+            return res.status(403).json({message: "Role denied"});
+        } 
+
+        next();
+    }   
+}
