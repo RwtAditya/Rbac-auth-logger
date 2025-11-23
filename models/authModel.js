a505fa1d89cbf175f065a1e28ec8d678ce9b4156
unknown <117914221+RwtAditya@users.noreply.github.com>
2025-11-23 19:22:18 +0530

    Model that talks to database for logging or registering user

---
+++ models/authModel.js
@@ -0,0 +1,48 @@
+const pool = require("../db/db");
+
+//users tabel queries
+exports.registerUser = async (name, email, password) => {
+    const query = "INSERT INTO users (name, email, password_hash) VALUES ($1, $2, $3) RETURNING id";
+    const result = await pool.query(query, [name, email, password]);
+
+    return result.rows[0];
+}
+
+exports.findByEmail = async (email) => {
+    const query = "SELECT * FROM users WHERE email =$1";
+    const result = await pool.query(query, [email]);
+
+    return result.rows[0];
+}
+
+
+//user_roles table queries
+exports.asignUserRole = async (userId, roleId) => {
+    const query = "INSERT INTO user_roles (user_id, role_id) VALUES ($1,$2) RETURNING *";
+    const result = await pool.query(query, [userId, roleId]);
+
+    return result.rows[0];
+}
+
+exports.getUserRoleId = async (userId) => {
+    const query = "SELECT role_id FROM user_roles WHERE user_id = $1";
+    const result = await pool.query(query, [userId]);
+
+    return result.rows[0];
+}
+
+
+//roles table queries
+exports.getRoleId = async (role) => {
+    const query = "SELECT id FROM roles WHERE name = $1";
+    const result = await pool.query(query, [role]);
+
+    return result.rows[0];
+}
+
+exports.getRoleName = async (roleId) => {
+    const query = "SELECT name FROM roles WHERE id = $1";
+    const result = await pool.query(query, [roleId]);
+
+    return result.rows[0];
+}
