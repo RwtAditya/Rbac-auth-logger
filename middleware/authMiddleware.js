62bb643f9469b875eaa5e0ba20ec62f33085a146
unknown <117914221+RwtAditya@users.noreply.github.com>
2025-11-25 08:25:53 +0530

    no changes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const verifyToken  = (req, res, next) => {
+exports.verifyToken  = (req, res, next) => {
     const token = req.headers["authorization"]?.split[" "][1];
     console.log(token);
     if(!token) {
