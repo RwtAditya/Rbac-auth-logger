c546ec57e2bba59e8a56966965d63190f3e9b48e
unknown <117914221+RwtAditya@users.noreply.github.com>
2025-11-25 08:21:55 +0530

    Added routers for routes for auth, admin, moderator, user and logs

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const authRouter = require("./routes/authRouter");
+const userRouter = require("./routes/userRouter");
+const adminRouter = require("./routes/adminRouter");
+const moderatorRouter = require("./routes/moderatorRouter");
+const logsRouter = require("./routes/logsRouter");
 const dotenv = require("dotenv");
 
 
@@ -7,9 +12,12 @@ dotenv.config();
 
 app.use(express.json());
 
-app.get("/health", (req, res) => {
-    res.send("Hello");
-})
+app.use("/api/auth", authRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/moderator", moderatorRouter);
+app.use("/api/user", userRouter);
+app.use("/api/logs", logsRouter);
+
 
 PORT = process.env.PORT || 3000;
 app.listen(PORT, (err) => {
