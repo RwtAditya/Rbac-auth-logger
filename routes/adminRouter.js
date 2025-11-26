3ee434c475345cf96bd48981a70923b637bc2c3b
unknown <117914221+RwtAditya@users.noreply.github.com>
2025-11-26 08:17:19 +0530

    Fix imports and route parameters in adminRouter
    - Remove unused logsRouter import
    - Fix middleware imports to use destructuring
    - Add :id parameter to delete-user route

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,14 +1,13 @@
 const { Router } = require("express");
 const adminController = require("../controllers/adminController");
-const verifyToken = require("../middleware/authMiddleware");
-const requireRole = require("../middleware/roleMiddleware");
+const {verifyToken} = require("../middleware/authMiddleware");
+const {requireRole} = require("../middleware/roleMiddleware");
 
 const adminRouter = Router();
 
-
 adminRouter.get("/all-users",verifyToken, requireRole("admin"), adminController.getUsers);
 adminRouter.put("/update-role", verifyToken, requireRole("admin"), adminController.updateRole);
-adminRouter.delete("/delete-user", verifyToken, requireRole("admin"), adminController.deleteUser);
+adminRouter.delete("/delete-user/:id", verifyToken, requireRole("admin"), adminController.deleteUser);
 
 
 module.exports = adminRouter;
