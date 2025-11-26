60434ac501474cdf1d2701a3ef03c44d0c01dff9
unknown <117914221+RwtAditya@users.noreply.github.com>
2025-11-26 08:20:33 +0530

    Add const keyword to PORT variable in server.js
    - Declare PORT with const to prevent accidental reassignment

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use("/api/user", userRouter);
 app.use("/api/logs", logsRouter);
 
 
-PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, (err) => {
     if(err) {
         throw err;
